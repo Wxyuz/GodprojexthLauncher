afd423ee18e28e6a042baf023f5c62ff2ed4a254
Wxyuz <discordpayu@gmail.com>
2026-05-17 07:51:24 +0700

    Update download URL for FreedxmLauncher zip

--- run.ps1
+++ run.ps1
@@ -4,7 +4,7 @@
 
 $ErrorActionPreference = "Stop"
 
-$ZipUrl = "PUT_YOUR_FREEDXMLAUNCHER_ZIP_DIRECT_LINK_HERE"
+$ZipUrl = "https://github.com/Wxyuz/GodprojexthLauncher/releases/latest/download/FreedxmLauncher1.zip"
 $Sha256 = "1A9262B9DC9A00EF662325EE14466246354C929F75636F65D85507520811B075"
 $InstallDir = Join-Path $env:LOCALAPPDATA "FreedxmLauncher"
 $TempRoot = Join-Path $env:TEMP "FreedxmLauncherInstall"
